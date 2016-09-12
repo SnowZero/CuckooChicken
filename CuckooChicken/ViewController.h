d2caf1e618e18913e005c4e93148d984b6359c74
“snow” <snos@snosde-macbook-pro.local>
2016-09-12 19:59:40 +0800

    加入SKSpriteKit

---
+++ CuckooChicken/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CuckooChicken
+//
+//  Created by Snos on 2016/9/12.
+//  Copyright © 2016年 Snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CuckooChicken/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  CuckooChicken
+//
+//  Created by Snos on 2016/9/12.
+//  Copyright © 2016年 Snow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"dd");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
