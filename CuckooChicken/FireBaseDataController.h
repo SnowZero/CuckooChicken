bd47d8037fab5e66c638ea6f78aa25c2d772526d
魏凡皓 <weifanhao@weifanhaode-MacBook-Air.local>
2016-10-18 17:04:51 +0800

    2016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-182016-10-18

---
+++ CuckooChicken/FireBaseDataController.h
@@ -0,0 +1,13 @@
+//
+//  FireBaseDataController.h
+//  CuckooChicken
+//
+//  Created by 魏凡皓 on 2016/10/12.
+//  Copyright © 2016年 Snow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FireBaseDataController : NSObject
+
+@end
---
+++ CuckooChicken/FireBaseDataController.m
@@ -0,0 +1,13 @@
+//
+//  FireBaseDataController.m
+//  CuckooChicken
+//
+//  Created by 魏凡皓 on 2016/10/12.
+//  Copyright © 2016年 Snow. All rights reserved.
+//
+
+#import "FireBaseDataController.h"
+
+@implementation FireBaseDataController
+
+@end
